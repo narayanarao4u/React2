e2582204d06ccb77067d13ecf2969c0c49641ba7
Narayana Rao <narayanarao4u@yahoo.com>
2023-07-03 22:51:49 +0530

    initial

---
+++ src/ClassComp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ClassComp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>This Class Component</h2>
+      </div>
+    )
+  }
+}
+
+export default ClassComp;

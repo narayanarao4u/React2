e3f0034be31de0d744aba1581966a3ad24bd02e9
narayanarao4u <narayanarao4u@yahoo.com>
2023-07-03 23:14:40 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+
 function App() {
   return (
     <div>
-        <h1>Hello ReactJS</h1>
+      <h1>Hello ReactJS</h1>
+      <h2>TesEst</h2>
     </div>
-  )
+  );
 }
 
 export default App;

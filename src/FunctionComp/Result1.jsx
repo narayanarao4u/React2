dfcff386966c444ec9f78be6c1fdf8ef7bb949ec
Narayana Rao <narayanarao4u@yahoo.com>
2023-07-10 20:01:46 +0530

    update Result1.jsx, StudentDetails.js and src/index.css created testing Branch

--- src/FunctionComp/Result1.jsx
+++ src/FunctionComp/Result1.jsx
@@ -13,9 +13,31 @@ class Result1 extends Component {
         { subject: "Physics", score: 30 },
         { subject: "Chemistry", score: 50 },
       ],
+      revalueApply: false,
+      message: "Apply for Revaluation",
+      count: 0,
     };
   }
 
+  btnClick() {
+    let myState = { ...this.state };
+    myState.revalueApply = true;
+    myState.message = "Applied for Revaluation";
+    this.setState(
+      {
+        revalueApply: myState.revalueApply,
+        message: myState.message,
+        count: myState.count + 1,
+      },
+      () => {
+        console.log(this.state.count);
+      }
+    );
+  }
+
+  clickbtn() {
+    console.log("button clicked");
+  }
   render() {
     return (
       <div>
@@ -24,12 +46,19 @@ class Result1 extends Component {
           classStudy={this.state.classStudy}
         />
         <ol>
-          {this.state.marks.map((o) => (
-            <li>
+          {this.state.marks.map((o, i) => (
+            <li key={i}>
               <Marks subject={o.subject} score={o.score} />
             </li>
           ))}
         </ol>
+        <span>
+          {" "}
+          {this.state.message}{" "}
+          {this.state.count > 0 ? this.state.count + "times" : ""}
+        </span>{" "}
+        <button onClick={this.clickbtn}>Click</button>
+        <button onClick={() => this.btnClick()}>Apply</button>
       </div>
     );
   }

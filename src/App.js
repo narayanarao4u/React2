d01437341070c944ea9198f9b811b196bf0995d7
Narayana Rao <narayanarao4u@yahoo.com>
2023-07-14 18:19:13 +0530

    Calculator update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Result1 from "./FunctionComp/Result1";
+import Calculator from "./Calculator/Calculator";
 
 function App() {
   return (
     <div>
-      <Result1 />
+      {/* <Result1 /> */}
+      <hr />
+      <Calculator />
     </div>
   );
 }
